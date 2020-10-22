b815a9b04464d290ef95f93076284f8a2ee4dca9
shuaibird <shuaibird@icloud.com>
2020-10-22 23:43:51 +0800

    Feat: styling adds grid

--- src/Components/Input.svelte
+++ src/Components/Input.svelte
@@ -18,23 +18,36 @@
 </script>
 
 <style>
+  .grid {
+    border: 5px solid black;
+  }
   .row {
     display: flex;
+  }
+  .row:nth-child(3),
+  .row:nth-child(6) {
+    border-bottom: 3px solid black;
   }
   input {
     flex: 1;
     width: 40px;
     height: 40px;
     text-align: center;
+    border: 1px solid #777;
+    font-size: 16px;
   }
 
   input::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
   }
+  input:nth-child(3),
+  input:nth-child(6) {
+    border-right: 5px solid black;
+  }
 </style>
 
-<form autocomplete="off" bind:this={form}>
+<form autocomplete="off" bind:this={form} class="grid">
   {#each ROWS as row}
     <div class="row">
       {#each row as square}
--- src/Components/Output.svelte
+++ src/Components/Output.svelte
@@ -6,32 +6,45 @@
 </script>
 
 <style>
+  .grid {
+    border: 5px solid black;
+  }
   .row {
     display: flex;
+  }
+  .row:nth-child(3),
+  .row:nth-child(6) {
+    border-bottom: 3px solid black;
   }
   .square {
     flex: 1;
     width: 40px;
     height: 40px;
     text-align: center;
-    border: 1px solid black;
+    border: 1px solid #777;
     line-height: 40px;
   }
   .square.emphasized {
     color: rgba(238, 18, 2, 0.712);
   }
+  .square:nth-child(3),
+  .square:nth-child(6) {
+    border-right: 5px solid black;
+  }
 </style>
 
 {#if answer}
-  {#each ROWS as row}
-    <div class="row">
-      {#each row as square}
-        <div class="square" class:emphasized={!puzzle[square]}>
-          {answer[square]}
-        </div>
-      {/each}
-    </div>
-  {/each}
+  <div class="grid">
+    {#each ROWS as row}
+      <div class="row">
+        {#each row as square}
+          <div class="square" class:emphasized={!puzzle[square]}>
+            {answer[square]}
+          </div>
+        {/each}
+      </div>
+    {/each}
+  </div>
 {:else}
   Ooops, I don't have an answer for this puzzle.
 {/if}
